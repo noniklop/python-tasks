3adcf73024d0c151e769132468af4e69f2a03640
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2022-10-13 14:50:40 +0300

    Changed regex which find ip. Moved input to 'if main' block

--- ip_validation.py
+++ ip_validation.py
@@ -1,8 +1,6 @@
 import socket
 import re
 
-input_ip = input("Insert your ip: ")
-
 
 def check_valid_ip_socket(ip):
     try:
@@ -10,20 +8,18 @@ def check_valid_ip_socket(ip):
         status_ip = True
     except socket.error:
         status_ip = False
-    return print("Status IP: ", status_ip)
+    return status_ip
 
 
 def check_valid_ip_re(ip):
-    bytes_ip = ip.split(".")
-    check_ip = re.match(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", ip)
-    for ip_byte in bytes_ip:
-        if 0 < int(ip_byte) > 255 or check_ip is None:
-            return print("Invalid ip: ", ip)
-        if len(bytes_ip) != 4:
-            return print("Invalid ip: ", ip)
-    print("Valid ip")
+    check_ip = re.match(r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b", ip)
+    if check_ip is not None:
+        print("Correct IP: ", check_ip.group(0))
+    else:
+        print("Invalid IP")
 
 
 if __name__ == "__main__":
-    check_valid_ip_socket(input_ip)
+    input_ip = input("Insert your ip: ")
+    print("Status IP: ", check_valid_ip_socket(input_ip))
     check_valid_ip_re(input_ip)
