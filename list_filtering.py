f3fade39e89e5936c5e32a157aca198da3c3a13e
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2022-10-13 14:52:18 +0300

    Moved list to 'if main' block. Changed return in functions. Changed type(item) to isinstance()

--- list_filtering.py
+++ list_filtering.py
@@ -1,23 +1,21 @@
-l = [1, 2, '3', 4, None, 10, 33, 'Python', -37.5]
-
-
-def filtering_integer_by_for():
-    for item in l:
-        if type(item) == int:
+def filtering_integer_by_for(list_content):
+    for item in list_content:
+        if isinstance(item, int):
             print(item)
 
 
-def filtering_integer_by_list_comprehensions():
-    integer_list = [item for item in l if type(item) == int]
-    return print(integer_list)
+def filtering_integer_by_list_comprehensions(list_content):
+    integer_list = [item for item in list_content if isinstance(item, int)]
+    return integer_list
 
 
 def filtering_integer_by_filter_and_lambda(list_content):
-    result = list(filter(lambda x: type(x) == int, list_content))
-    return print(result)
+    result = list(filter(lambda x: isinstance(x, int), list_content))
+    return result
 
 
 if __name__ == '__main__':
-    filtering_integer_by_for()
-    filtering_integer_by_list_comprehensions()
-    filtering_integer_by_filter_and_lambda(l)
+    l = [1, 2, '3', 4, None, 10, 33, 'Python', -37.5]
+    filtering_integer_by_for(l)
+    print(filtering_integer_by_list_comprehensions(l))
+    print(filtering_integer_by_filter_and_lambda(l))
