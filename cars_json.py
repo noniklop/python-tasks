3417baa8dd8e1cbb84659da6c30186bde45544cd
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2022-10-17 12:51:17 +0300

    Added functions

--- cars_json.py
+++ cars_json.py
@@ -1,14 +1,22 @@
 import csv
 import json
 
-list_cars = []
-with open('cars.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        list_cars.append(row)
 
-cars_json = json.dumps(list_cars, indent=2)
-print(cars_json)
+def read_csv():
+    with open('cars.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            list_cars.append(row)
 
-with open('cars.json', "w") as json_file:
-    json_file.write(cars_json)
+    cars_json = json.dumps(list_cars, indent=2)
+    return cars_json
+
+
+def write_json(cars_json):
+    with open('cars.json', "w") as json_file:
+        json_file.write(cars_json)
+
+
+if __name__ == '__main__':
+    list_cars = []
+    write_json(read_csv())
