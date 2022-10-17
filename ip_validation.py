6340d39b0d3a5c77479b3365369fb9f48969b277
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2022-10-17 13:01:57 +0300

    Did that function return true or false. Removed input

--- ip_validation.py
+++ ip_validation.py
@@ -14,12 +14,12 @@ def check_valid_ip_socket(ip):
 def check_valid_ip_re(ip):
     check_ip = re.match(r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b", ip)
     if check_ip is not None:
-        print("Correct IP: ", check_ip.group(0))
+        return True
     else:
-        print("Invalid IP")
+        return False
 
 
 if __name__ == "__main__":
-    input_ip = input("Insert your ip: ")
-    print("Status IP: ", check_valid_ip_socket(input_ip))
-    check_valid_ip_re(input_ip)
+    target_ip = '192.168.0.1'
+    print("Status IP: ", check_valid_ip_socket(target_ip))
+    print(f'Assert {target_ip} is', check_valid_ip_re(target_ip))
