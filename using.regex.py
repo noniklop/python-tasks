fb3ff94de789dcb43cac04f9b69e8b3e4ab37add
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2022-10-19 19:06:43 +0300

    Added lambda in re.sub. Did small corrections

--- using.regex.py
+++ using.regex.py
@@ -1,16 +1,14 @@
 import re
 
-text = """Из 35 футболистов, забивших как минимум 7 голов на чемпионатах мира, только у троих футболистов средний 
-        показатель превышает 2 гола за игру. Эти 35 игроков представляют 14 футбольных сборных"""
+
+def multiplied(target_text):
+    final_text = re.sub(r'\b\d+\b', lambda number: str(int(number.group()) * 2), target_text)
+
+    return final_text
 
 
-def multiplied(matchobj):
-    number = matchobj.group(0)
-    multiplied_number = int(number) * 2
-    string_number = str(multiplied_number)
-    return string_number
-
-
-final_text = re.sub(r'\b\d+\b', multiplied, text)
-print(text)
-print(final_text)
+if __name__ == '__main__':
+    text = """Из 35 футболистов, забивших как минимум 7 голов на чемпионатах мира, только у троих футболистов средний 
+            показатель превышает 2 гола за игру. Эти 35 игроков представляют 14 футбольных сборных"""
+    print(text)
+    print(multiplied(text))
