e624c3359376a54163f36ca669600363bc69f2c9
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2022-10-19 19:50:56 +0300

    Added timeit decorator

--- multiprocessing_task.py
+++ multiprocessing_task.py
@@ -1,23 +1,33 @@
 import multiprocessing
+from functools import wraps
 import time
 
 N = 300000000
 
 
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__}{args} {kwargs} Took {round(total_time, 3)}')
+        return result
+
+    return timeit_wrapper
+
+@timeit
 def simple_iteration():
-    start = time.time()
     res = 0
     for i in range(N):
         if i % 3 == 0 or i % 5 == 0:
             res += i
-    end = time.time()
-    duration = round(end - start, 3)
-    print(f'simple_iteration duration is {duration} sec')
+
     return res
 
-
+@timeit
 def several_for_loops():
-    start = time.time()
     res = 0
     for i in range(3, N, 3):
         res += i
@@ -25,14 +35,10 @@ def several_for_loops():
         res += i
     for i in range(15, N, 15):
         res -= i
-    end = time.time()
-    duration = round(end - start, 3)
-    print(f'several_for_loops duration is {duration} sec')
     return res
 
-
+@timeit
 def iterate_over_fifteen():
-    #start = time.time()
     range_diff = [0, 3, 5, 6, 9, 10, 12]
     res = 0
     for i in range(0, N, 15):
@@ -41,22 +47,17 @@ def iterate_over_fifteen():
             if v >= N:
                 break
             res += v
-    #end = time.time()
-    #duration = round(end - start, 3)
-    #print(f'iterate_over_fifteen duration is {duration} sec')
+
     return res
 
-
+@timeit
 def math_formula():
-    start = time.time()
     upper = N - 1
     threes = int(3 * (upper / 3) * ((upper / 3) + 1) / 2)
     fives = int(5 * (upper / 5) * ((upper / 5) + 1) / 2)
     fifteens = int(15 * (upper / 15) * ((upper / 15) + 1) / 2)
     res = threes + fives - fifteens
-    end = time.time()
-    duration = round(end - start, 3)
-    print(f'math_formula duration is {duration} sec')
+
     return res
 
 
@@ -77,7 +78,5 @@ def run_all_calculations_in_parallel():
     # Print execution time of each function
 
 
-
 if __name__ == '__main__':
     run_all_calculations_in_parallel()
-
