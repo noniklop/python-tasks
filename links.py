68488a5a6f34d33a171db9c035db3ac479f550f9
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2022-11-10 12:16:01 +0200

    Changed output in json

--- links.py
+++ links.py
@@ -5,21 +5,29 @@ import threading
 
 
 def check_url(url):
+    result_list = {}
     try:
         status_url = requests.get(url)
         print("Target: ", url)
         print("Status: ", status_url.ok, '\n')
-        result_list[url] = status_url.ok
+        result_list['url'] = url
+        result_list['is_ok'] = status_url.ok
+        result_list['status_code'] = status_url.status_code
+
     except requests.exceptions.ConnectionError:
         status_url = False
         print("Target: ", url)
         print("Status: ", status_url, '\n')
-        result_list[url] = status_url
+        result_list['url'] = url
+        result_list['is_ok'] = status_url
+        result_list['status_code'] = 'null'
+
+    final_result.append(result_list)
 
 
 if __name__ == '__main__':
     links_list = []
-    result_list = {}
+    final_result = []
     with open('links.txt', 'r') as links:
         for item in links:
             links_list.append(item.replace('\n', ''))
@@ -28,7 +36,7 @@ if __name__ == '__main__':
         t = threading.Thread(target=check_url(item))
         t.start()
 
-json_object = json.dumps(result_list, indent=2)
+json_object = json.dumps(final_result, indent=2)
 
 with open('result_links.json', "w") as json_file:
     json_file.write(json_object)
