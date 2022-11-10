ea12aba3e882dbaca9114825a196d371ee74a89e
Vladyslav Melnychuk <80819379+noniklop@users.noreply.github.com>
2022-11-10 18:48:59 +0200

    Corrected accert. Added assert check other ip

--- ip_validation.py
+++ ip_validation.py
@@ -12,7 +12,8 @@ def check_valid_ip_socket(ip):
 
 
 def check_valid_ip_re(ip):
-    check_ip = re.match(r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b", ip)
+    check_ip = re.match(
+        r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b", ip)
     if check_ip is not None:
         return True
     else:
@@ -22,4 +23,11 @@ def check_valid_ip_re(ip):
 if __name__ == "__main__":
     target_ip = '192.168.0.1'
     print("Status IP: ", check_valid_ip_socket(target_ip))
-    assert target_ip, check_valid_ip_re(target_ip)
+    # assert target_ip, check_valid_ip_re(target_ip)
+
+    assert check_valid_ip_re('') is False
+    assert check_valid_ip_re('192.168.0.1') is True
+    assert check_valid_ip_re('0.0.0.1') is True
+    assert check_valid_ip_re('10.100.500.32') is False
+    assert check_valid_ip_re('700') is False
+    assert check_valid_ip_re('127.0.0.1') is True
